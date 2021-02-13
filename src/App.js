569359335ccc99c6ea9c5ec9599de4966055d47f
cspark0610 <cpark0610@gmail.com>
2021-02-13 15:22:03 -0300

    13/2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Movie from "./Movie";
 
 const App = ()=>{
+  //quiero renderizar inicialmente en el home las peliculas mas populares con FEATURED_API
 	const FEATURED_API =
 		"https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 
@@ -10,12 +11,14 @@ const App = ()=>{
 		"https://api.themoviedb.org/3/discover/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 	const [movies, setMovies] = useState([]);
+  //movies es un array con estado inicial [] que luego sera rellenado 
+  //de acuerdo al useEffect
 	const [searchMovie, setSearchMovie] = useState("");
 
 	const getMovies = () => {
 		fetch(FEATURED_API)
-			.then((res) => res.json())
-			.then((data) => {
+			.then( res => res.json())
+			.then( data  => {
 				console.log(data);
 				setMovies(data.results);
 			});
@@ -25,14 +28,17 @@ const App = ()=>{
 		getMovies(FEATURED_API);
 	}, []);
 
-	const handleOnChange = (e) => {
+	const handleInput = (e) => {
+    //https://reactgo.com/react-get-input-value/
 		setSearchMovie(e.target.value);
 	};
 
-	const handleOnSubmit = (e) => {
+	const handleSubmit = (e) => {
 		e.preventDefault();
 		if (searchMovie) {
-			getMovies(FEATURED_API + searchMovie);
+			getMovies(SEARCH_API + searchMovie);
+      //si el usuario apreta enter y hace la busqueda 
+      //seteo la var searchMovie a ""
 			setSearchMovie("");
 		}
 	};
@@ -40,18 +46,19 @@ const App = ()=>{
 	return (
 		<>
 			<header className='header'>
-				<form onSubmit={handleOnSubmit} />
+				<form onSubmit={handleSubmit}>
 				<input
 					className='search'
 					type='search'
 					placeholder='Search...'
 					value={searchMovie}
-					onChange={handleOnChange}
+					onChange={handleInput}
 				/>
+        </form>
 			</header>
 
-			<div className='movie-container'>
-				{movies.length > 0 && movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      <div className='movie-container'>
+			{movies.length > 0 && movies.map((movie) => <Movie key={movie.id} {...movie} />)}
 			</div>
 		</>
 	);
--- src/Movie.jsx
+++ src/Movie.jsx
@@ -3,11 +3,11 @@ const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const URL ="https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
 
 export default Movie = ({ title, poster_path, overview, vote_average }) => (
-	<div className='movie-container'>
+	<div className='movie'>
 		<img src={poster_path ? IMG_API + poster_path : URL} alt={title} />
 		<div className='movie-info'>
 			<h3>{title}</h3>
-			<span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
+			<span className={`${setVoteClass(vote_average)}`}>{vote_average}</span>
 		</div>
 
 		<div className='movie-over'>
@@ -16,6 +16,8 @@ export default Movie = ({ title, poster_path, overview, vote_average }) => (
 		</div>
 	</div>
 );
+// retorno con esta funcion setVoteClass el className 'tag green' 
+//'tag orange' y 'tag red' de acuerdo al vote_average
 
 const setVoteClass = (vote) => {
 	if (vote > 8) {
