bd0f3d07135e57310d8fe85f09d6deba2372f11a
cspark0610 <cpark0610@gmail.com>
2021-02-13 15:24:18 -0300

    13/2

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const URL ="https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
 
-export default Movie = ({ title, poster_path, overview, vote_average }) => (
+const Movie = ({ title, poster_path, overview, vote_average }) => (
 	<div className='movie'>
 		<img src={poster_path ? IMG_API + poster_path : URL} alt={title} />
 		<div className='movie-info'>
@@ -16,6 +16,8 @@ export default Movie = ({ title, poster_path, overview, vote_average }) => (
 		</div>
 	</div>
 );
+
+export default Movie;
 // retorno con esta funcion setVoteClass el className 'tag green' 
 //'tag orange' y 'tag red' de acuerdo al vote_average
 
